df0f2fec203b555f41504344c1b1cd53a25e481e
kyleduval42 <kyle.duval22@gmail.com>
2019-10-22 11:13:56 -0700

    Changes

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -4,6 +4,6 @@ public class SemanticAnalysis {
     public static void main(String[] args){
       String wholeText = TextLib.readFileAsString("C:\\Users\\kduval139\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
       System.out.println(wholeText);
-      String question = wholeText.substring();
+      String question = wholeText.substring(1,2);
     }
 }
