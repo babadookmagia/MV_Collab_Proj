68d2e8e4cf2b648b8054c9269f7abfe6c948fc62
kyleduval42 <kyle.duval22@gmail.com>
2019-10-22 11:12:23 -0700

    Changes

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -1,12 +1,9 @@
+import javax.xml.soap.Text;
+
 public class SemanticAnalysis {
-    public static void main(String[] args) {
-
-
-
-
-
-
-
-
+    public static void main(String[] args){
+      String wholeText = TextLib.readFileAsString("C:\\Users\\kduval139\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
+      System.out.println(wholeText);
+      String question = wholeText.substring();
     }
 }
