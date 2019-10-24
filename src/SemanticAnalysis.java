204d1380e234ebef038e6345fb15425e31970c53
kyleduval42 <kyle.duval22@gmail.com>
2019-10-24 10:16:50 -0700

    Changes

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -1,9 +1,24 @@
 import javax.xml.soap.Text;
 
 public class SemanticAnalysis {
-    public static void main(String[] args){
-      String wholeText = TextLib.readFileAsString("C:\\Users\\kduval139\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
-      System.out.println(wholeText);
-      String question = wholeText.substring(1,2);
+    public static void main(String[] args) {
+        Document corpus = Document.findDocument("C:\\Users\\kduval139\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
+        String text = corpus.getText();
+        String question = findquestion(text);
+        String answerTxt = text.substring(text.indexOf("Answer") + 7);
+        String[] answers = seperateAnswers(answerTxt);
+    }
+
+    private static String[] seperateAnswers(String answerTxt) {
+       String[] answers = answerTxt.split("Answer");
+        for (String anser: answers) {
+        }
+        return answers;
+    }
+
+    private static String findquestion(String text) {
+        int startindex = text.indexOf("Question") + 8;
+        int endindex = text.indexOf("Answer");
+        return text.substring(startindex, endindex);
     }
 }
