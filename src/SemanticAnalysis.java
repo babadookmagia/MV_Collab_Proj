45286fbfaf89645a3c15b1f135b7aa04e9e3b428
kyleduval42 <51682638+kyleduval42@users.noreply.github.com>
2019-10-23 20:01:07 -0700

    Semantic analysis updated

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -1,3 +1,4 @@
+import javax.print.Doc;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
@@ -7,31 +8,40 @@ import java.util.Scanner;
 
 public class SemanticAnalysis {
     public static void main(String[] args) {
-        Document.findDocument("C:\\Users\\kyled\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
+        Document corpus = Document.findDocument("C:\\Users\\kyled\\IdeaProjects\\MV_Collab_Proj\\data\\Corpus (Testing) - Copy.txt");
+        String text = corpus.getText();
+        String question = findquestion(text);
+        System.out.println(question);;
+    }
 
+    private static String findquestion(String text) {
+        int startIndex = text.indexOf("Question") + 8;
+        int endIndex = text.indexOf("Answer");
+        return text.substring(startIndex, endIndex);
     }
 
     public int countOccurences(String text, String target) {
         int count = 0;
 
-        for(int index = text.indexOf(target); index != -1; index = text.indexOf(target, index + 1)) {
+        for (int index = text.indexOf(target); index != -1; index = text.indexOf(target, index + 1)) {
             ++count;
         }
 
         return count;
     }
 
-    public ArrayList<Integer> occuranceIndex(String text,String target) {
+    public ArrayList<Integer> occuranceIndex(String text, String target) {
         ArrayList<Integer> answer = new ArrayList();
 
-        for(int index = text.indexOf(target); index != -1; index = text.indexOf(target, index + 1)) {
+        for (int index = text.indexOf(target); index != -1; index = text.indexOf(target, index + 1)) {
             answer.add(index);
         }
 
         return answer;
     }
-    public boolean coOccurWithinDist(String text,String target_1, String target_2, int distance) {
-        for(int i = 0; i < text.length() - distance; ++i) {
+
+    public boolean coOccurWithinDist(String text, String target_1, String target_2, int distance) {
+        for (int i = 0; i < text.length() - distance; ++i) {
             String checkArea = text.substring(i, i + distance + 1);
             if (checkArea.contains(target_1) && checkArea.contains(target_2)) {
                 return true;
@@ -40,15 +50,17 @@ public class SemanticAnalysis {
 
         return false;
     }
+
     private static boolean wordsContain(String input, String search) {
         return (input.contains(search));
     }
+
     private static ArrayList<String> getUniqueWords(ArrayList<String> words) {
         ArrayList<String> uniqueWords = new ArrayList();
         Iterator var2 = words.iterator();
 
-        while(var2.hasNext()) {
-            String word = (String)var2.next();
+        while (var2.hasNext()) {
+            String word = (String) var2.next();
             if (!uniqueWords.contains(word)) {
                 uniqueWords.add(word);
             }
@@ -69,7 +81,7 @@ public class SemanticAnalysis {
     }
 
     private static void addtoarraylist(ArrayList<String> answer, String[] words) {
-        for (String word:words) {
+        for (String word : words) {
             if (!word.equals("")) {
                 answer.add(word);
             }
@@ -79,7 +91,7 @@ public class SemanticAnalysis {
     private static String removePunctuation(String sentence) {
         String answer = "";
 
-        for(int i = 0; i < sentence.length(); ++i) {
+        for (int i = 0; i < sentence.length(); ++i) {
             String letter = sentence.substring(i, i + 1);
             if (isletter(letter)) {
                 answer = answer + letter;
